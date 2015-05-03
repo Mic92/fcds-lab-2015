3262f34397b54776d34bd8b36f21a89ab40e84e8
Jörg Thalheim <joerg@higgsboson.tk>
2015-05-03 19:08:18 +0200

    get bucketsort working

--- bucketsort/bucketsort.go
+++ bucketsort/bucketsort.go
@@ -1,97 +1,104 @@
 package bucketsort
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
 const (
 	NUMBER_OF_BUCKETS = 94
+	WORD_LENGTH       = 7
 )
 
-func Sort(data []byte, length int) []int {
-	var buckets [NUMBER_OF_BUCKETS][]int
-	size := len(data)
-	var idx byte
+func sort(data []byte, bucket []int, length int) {
+	for j, key := range bucket {
+		i := j - 1
+		bIdx := key * length
+		b := data[bIdx:(bIdx + length)]
 
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Printf("%d\n", idx)
+		for i >= 0 {
+			aIdx := bucket[i] * length
+			a := data[aIdx:(aIdx + length)]
+			if bytes.Compare(a, b) <= 0 {
+				break
+			}
+			bucket[i+1] = bucket[i]
+			i--
 		}
-	}()
+		bucket[i+1] = key
+	}
+}
 
-	returns := make([]int, len(data))
+func Sort(data []byte, wordLength int) []int {
+	var buckets [NUMBER_OF_BUCKETS][]int
+	size := len(data) / wordLength
+
+	returns := make([]int, size)
+	step := size / NUMBER_OF_BUCKETS
 	for i := range buckets {
-		offset := (i * size) / NUMBER_OF_BUCKETS
-		buckets[i] = returns[offset:offset:len(returns)]
+		offset := i * step
+		buckets[i] = returns[offset:offset : offset+step]
 	}
 
-	elements := size / length
-	for i := 0; i < elements; i++ {
-		key := data[i*length] - 0x21
-		//fmt.Printf("append(buckets[%d], %d (data[%d]=%s))\n", key, i, i*length, data[i*length:i*length+length])
-		//fmt.Printf("returns=%v\n", returns)
-		//fmt.Printf("buckets[%d] -> %v\n", key, buckets[key])
-		//idx = data[i*length]
-		fmt.Println(data[i*length : i*length+10])
-		idx = key
+	for i := 0; i < size; i++ {
+		key := data[i*wordLength] - 0x21
 		buckets[key] = append(buckets[key], i)
-		//fmt.Printf("buckets[%d] -> %v\n", key, buckets[key])
+	}
+
+	for _, bucket := range buckets {
+		sort(data, bucket, wordLength)
 	}
 
 	return returns
 }
 
-//#include <stdlib.h>
-//#include <string.h>
-//#include "bucketsort.h"
-//
-//#define N_BUCKETS 94
-//
-//typedef struct {
-//	long int *data;
-//	int length;
-//	long int total;
-//} bucket;
-//
-//void sort(char *a, bucket *bucket) {
-//	int j, i, length;
-//	long int key;
-//	length = bucket->length;
-//	for (j = 1; j < bucket->total; j++) {
-//		key = bucket->data[j];
-//		i = j - 1;
-//		while (i >= 0
-//				&& strcmp(a + bucket->data[i] * length, a + key * length) > 0) {
-//			bucket->data[i + 1] = bucket->data[i];
-//			i--;
-//		}
-//		bucket->data[i + 1] = key;
-//	}
-//}
-//
-//long int* bucket_sort(char *a, int length, long int size) {
-//
-//	long int i;
-//	bucket buckets[N_BUCKETS], *b;
-//	long int *returns;
-//
-//	// allocate memory
-//	returns = malloc(sizeof(long int) * size);
-//	for (i = 0; i < N_BUCKETS; i++) {
-//		buckets[i].data = returns + i * size / N_BUCKETS;
-//		buckets[i].length = length;
-//		buckets[i].total = 0;
-//	}
-//
-//	// copy the keys to "buckets"
-//	for (i = 0; i < size; i++) {
-//		b = &buckets[*(a + i * length) - 0x21];
-//		b->data[b->total++] = i;
-//	}
-//
-//	// sort each "bucket"
-//	for (i = 0; i < N_BUCKETS; i++)
-//		sort(a, &buckets[i]);
-//
-//	return returns;
-//}
+func readInput(in *os.File, wordLength int) ([]byte, error) {
+	r := bufio.NewReader(in)
+	var lineCount int
+	n, err := fmt.Fscanln(r, &lineCount)
+	if err != nil {
+		return nil, err
+	}
+	if n == 0 {
+		return []byte{}, nil
+	}
+
+	data := make([]byte, 0, lineCount*wordLength)
+	for {
+		line, isPrefix, err := r.ReadLine()
+		if isPrefix {
+			return nil, fmt.Errorf("line too long")
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("error while reading file %v: %v", in, err)
+		}
+		data = append(data, []byte(line)...)
+	}
+	return data, nil
+}
+
+func SortFile(in *os.File, out *os.File) error {
+	data, err := readInput(in, WORD_LENGTH)
+
+	if err != nil {
+		return err
+	}
+	for _, idx := range Sort(data, WORD_LENGTH) {
+		offset := idx * WORD_LENGTH
+		_, err = out.Write(data[offset : offset+WORD_LENGTH])
+		if err != nil {
+			return err
+		}
+		_, err = out.Write([]byte{'\n'})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}

46321e503a5f812544e4aaa905500332502fe327
Jörg Thalheim <joerg@higgsboson.tk>
2015-05-05 12:09:19 +0200

    fix test

--- bucketsort/bucketsort_test.go
+++ bucketsort/bucketsort_test.go
@@ -1,24 +1,35 @@
 package bucketsort
 
 import (
+	"math/rand"
 	sorting "sort"
 	"testing"
 )
 
 func TestSort(t *testing.T) {
+	rand.Seed(1)
 	data := make([]byte, 0)
 	for i := byte('~'); i >= byte('!'); i-- {
 		data = append(data, i)
-		for j := byte('a') + 6; j > byte('a'); j-- {
-			data = append(data, j)
+		for j := 0; j < 6; j++ {
+			data = append(data, byte(rand.Int()%256))
 		}
 	}
-	sorted := make([]string, len(data))
-	for _, idx := range Sort(data, 7) {
-		sorted = append(sorted, string(data[idx:idx+7]))
+	sorted := make([]string, len(data)/7)
+	for _, v := range Sort(data) {
+		word := []byte{
+			byte(v >> 48),
+			byte(v >> 40),
+			byte(v >> 32),
+			byte(v >> 24),
+			byte(v >> 16),
+			byte(v >> 8),
+			byte(v),
+			'\n',
+		}
+		sorted = append(sorted, string(word))
 	}
 	if !sorting.IsSorted(sorting.StringSlice(sorted)) {
-		t.Fatalf("expect data to be sorted, got: %v", sorted)
+		t.Fatalf("expect data to be sorted")
 	}
-	t.FailNow()
 }

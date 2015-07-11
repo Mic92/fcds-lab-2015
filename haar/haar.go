dbed2bf6d663dfd6a3a42ec3ba6492ec471e7f6c
Jörg Thalheim <joerg@higgsboson.tk>
2015-07-11 18:47:36 +0200

    add haar transformation

--- haar/haar.go
+++ haar/haar.go
@@ -2,6 +2,7 @@ package haar
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"syscall"
 )
@@ -9,46 +10,97 @@ import (
 func ProcessFile(in, out *os.File) error {
 	info, err := in.Stat()
 	if err != nil {
-		return fmt.Errorf("Error stat input file: %v", err)
+		return fmt.Errorf("Error stat input file '%s': %v", in.Name, err)
 	}
+	// check if out is a file
+	if _, err := out.Stat(); err != nil {
+		return fmt.Errorf("Error stat output file '%s': %v", out.Name, err)
+	}
+
+	// void *mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off);
+
+	if err := out.Truncate(info.Size()); err != nil {
+		return fmt.Errorf("Failed to resize output file '%s' to %d bytes: %v", out.Name, info.Size(), err)
+	}
+
 	// linux specific, but fast
-	syscall.Splice(int(in.Fd()), nil, int(out.Fd()), nil, int(info.Size()), 0)
+	if _, err := syscall.Splice(int(in.Fd()), nil, int(out.Fd()), nil, int(info.Size()), 0); err != nil {
+		return fmt.Errorf("Failed to copy input to output file: %v", err)
+	}
+
+	//buf, err := syscall.Mmap(int(out.Fd()), 0, int(info.Size()), syscall.PROT_WRITE|syscall.PROT_READ, syscall.MAP_SHARED)
+	//if err != nil {
+	//	return fmt.Errorf("Failed to mmap output file: %v", err)
+	//}
+
 	return nil
 }
 
-//	SQRT_2 = sqrt(2);
-//	for (s = size; s > 1; s /= 2) {
-//		mid = s / 2;
-//		// row-transformation
-//		for (y = 0; y < mid; y++) {
-//			for (x = 0; x < mid; x++) {
-//				a = pixel(x,y);
-//				a = (a+pixel(mid+x,y))/SQRT_2;
-//				d = pixel(x,y);
-//				d = (d-pixel(mid+x,y))/SQRT_2;
-//				pixel(x,y) = a;
-//				pixel(mid+x,y) = d;
-//			}
-//		}
-//
-//#ifdef DEBUG
-//		printf("after row-transformation: %lld\n", mid);
-//		print(pixels, size);
-//#endif
-//		// column-transformation
-//		for (y = 0; y < mid; y++) {
-//			for (x = 0; x < mid; x++) {
-//				a = pixel(x,y);
-//				a = (a+pixel(x,mid+y))/SQRT_2;
-//				d = pixel(x,y);
-//				d = (d-pixel(x,mid+y))/SQRT_2;
-//				pixel(x,y) = a;
-//				pixel(x,mid+y) = d;
-//			}
-//		}
-//
-//#ifdef DEBUG
-//		printf("after column-transformation: %lld\n", mid);
-//		print(pixels, size);
-//#endif
-//	}
+var sqrt_2 = math.Sqrt(2)
+
+type Image struct {
+	Pixels    []int
+	Dimension int64
+}
+
+// will it be inlined?
+func (i Image) at(x, y int64) int64 {
+	return int64(i.Pixels[y*i.Dimension+x])
+}
+
+// will it be inlined?
+func (i Image) to(x, y int64, value int) {
+	i.Pixels[y*i.Dimension+x] = value
+}
+
+func (i Image) print() {
+	for y := int64(0); y < i.Dimension; y++ {
+		for x := int64(0); x < i.Dimension; x++ {
+			fmt.Printf("%10d ", i.Pixels[y*i.Dimension+x])
+		}
+		fmt.Print("\n")
+	}
+}
+
+const debug = true
+
+func (i Image) Transform() {
+	if debug {
+		fmt.Printf("origin: %d\n", i.Dimension)
+		i.print()
+	}
+
+	for s := i.Dimension; s > 1; s /= 2 {
+		mid := s / 2
+		// row-transformation
+		for y := int64(0); y < mid; y++ {
+			for x := int64(0); x < mid; x++ {
+				pixel1 := int64(i.Pixels[y*i.Dimension+x])
+				pixel2 := int64(i.Pixels[y*i.Dimension+x+mid])
+				a := float64(pixel1+pixel2) / sqrt_2
+				d := float64(pixel1-pixel2) / sqrt_2
+				i.Pixels[y*i.Dimension+x] = int(a)
+				i.Pixels[y*i.Dimension+x+mid] = int(d)
+			}
+		}
+		if debug {
+			fmt.Printf("after row-transformation: %d\n", mid)
+			i.print()
+		}
+		// column-transformation
+		for y := int64(0); y < mid; y++ {
+			for x := int64(0); x < mid; x++ {
+				pixel1 := int64(i.Pixels[y*i.Dimension+x])
+				pixel2 := int64(i.Pixels[(y+mid)*i.Dimension+x])
+				a := float64(pixel1+pixel2) / sqrt_2
+				d := float64(pixel1-pixel2) / sqrt_2
+				i.Pixels[y*i.Dimension+x] = int(a)
+				i.Pixels[(y+mid)*i.Dimension+x] = int(d)
+			}
+		}
+		if debug {
+			fmt.Printf("after column-transformation: %d\n", mid)
+			i.print()
+		}
+	}
+}
--- haar/haar_test.go
+++ haar/haar_test.go
@@ -1,1 +1,63 @@
-package haar
+package haar_test
+
+import (
+	"github.com/Mic92/fcds-lab-2015/haar"
+	"testing"
+)
+
+var pixels_4x4_in = haar.Image{[]int{
+	55731,
+	27808,
+	30516,
+	51498,
+	56973,
+	9353,
+	41497,
+	24883,
+	62000,
+	40078,
+	65001,
+	48498,
+	28652,
+	7372,
+	9317,
+	41237,
+},
+	4,
+}
+var pixels_4x4_out = haar.Image{[]int{
+	126616,
+	13415,
+	17829,
+	-16751,
+	28337,
+	22330,
+	10943,
+	-10981,
+	-717,
+	11313,
+	65001,
+	48498,
+	28974,
+	11904,
+	9317,
+	41237,
+},
+	4,
+}
+
+func TestTransform(t *testing.T) {
+	pixels_4x4_in.Transform()
+	sucess := true
+	for i := range pixels_4x4_in.Pixels {
+		if pixels_4x4_in.Pixels[i] == pixels_4x4_out.Pixels[i] {
+			t.Logf("[%d]=%d", i, pixels_4x4_in.Pixels[i])
+		} else {
+			sucess = false
+			t.Logf("[%d] %d != %d", i, pixels_4x4_in.Pixels[i], pixels_4x4_out.Pixels[i])
+		}
+	}
+	if !sucess {
+		t.Fail()
+	}
+}
